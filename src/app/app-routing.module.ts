995e1de87ea6ed2a77f1a505efeb09fd73720526
Ashley Upson <ashmisterupson@gmail.com>
2019-03-13 19:06:23 +0000

    Initial build for server

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { IndexComponent } from "./index/index.component";
+import {Error404Component} from "./error404/error404.component";
+import {LoginComponent} from "./auth/login/login.component";
+import {RegisterComponent} from "./auth/register/register.component";
+import {ForgotComponent} from "./auth/forgot/forgot.component";
+import {ResetComponent} from "./auth/reset/reset.component";
+import {InviteComponent} from "./game/invite/invite.component";
+import {PlayComponent} from "./game/play/play.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: IndexComponent },
+  { path: 'auth', children:
+    [
+      { path: '', pathMatch: 'full', redirectTo: 'login' },
+      { path: 'login', component: LoginComponent },
+      { path: 'register', component: RegisterComponent },
+      { path: 'forgot', component: ForgotComponent },
+      { path: 'reset', component: ResetComponent }
+    ]
+  },
+  { path: 'game', children:
+    [
+      { path: '', pathMatch: 'full', redirectTo: 'play' },
+      { path: 'invite', component: InviteComponent },
+      { path: 'play', component: PlayComponent }
+    ]
+  },
+  { path: '**', component: Error404Component }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { ForgotComponent } from './auth/forgot/forgot.component';
+import { ResetComponent } from './auth/reset/reset.component';
+import { InviteComponent } from './game/invite/invite.component';
+import { PlayComponent } from './game/play/play.component';
+import { AdminComponent } from './admin/admin.component';
+import { ScoreboardComponent } from './scoreboard/scoreboard.component';
+import { IndexComponent } from './index/index.component';
+import { Error404Component } from './error404/error404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ForgotComponent,
+    ResetComponent,
+    InviteComponent,
+    PlayComponent,
+    AdminComponent,
+    ScoreboardComponent,
+    IndexComponent,
+    Error404Component
   ],
   imports: [
     BrowserModule,
